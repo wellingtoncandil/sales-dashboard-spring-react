e3b317139100e38e5331308f5e636f1f4c46a1e5
Wellington Candil <wcandil34@gmail.com>
2022-01-07 11:47:15 -0300

    Project created

---
+++ backend/src/main/java/com/wellington/wcvendas/WcvendasApplication.java
@@ -0,0 +1,13 @@
+package com.wellington.wcvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WcvendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WcvendasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/wellington/wcvendas/WcvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wellington.wcvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WcvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
