3cc7af178ae9c8e00cf43c2a9111e0f6d711e675
Wellington Candil <wcandil34@gmail.com>
2022-01-07 17:24:45 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,14 @@
+
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://www.linkedin.com/in/wcandil/" target="_blank" rel="noreferrer">Wellington Candil</a></p>
+                <p className="text-light"><small><strong>Sales Dashboard</strong><br />
+                    GitHub: <a href="https://github.com/wellingtoncandil" target="_blank" rel="noreferrer">@wellingtoncandil</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
