7b7840b0ca5ac8871c89c9efa9ec9a22aa378e69
Wellington Candil <wcandil34@gmail.com>
2022-01-10 13:12:55 -0300

    Domain model, database seed

---
+++ backend/src/main/java/com/wellington/wcvendas/entities/Seller.java
@@ -0,0 +1,74 @@
+package com.wellington.wcvendas.entities;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="tb_seller")
+public class Seller implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	
+	@OneToMany(mappedBy = "seller")
+	private List<Sale> sales = new ArrayList<>();
+	
+	public Seller(Long id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public Seller() {
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Seller other = (Seller) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	public List<Sale> getSales() {
+		return sales;
+	}
+	
+	
+}
