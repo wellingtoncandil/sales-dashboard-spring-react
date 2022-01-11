8d6385094030b69782403a8992aa65c46e255943
Wellington Candil <wcandil34@gmail.com>
2022-01-11 11:22:48 -0300

    Home, navigation

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -1,7 +1,7 @@
 
 const Footer = () => {
     return (
-        <footer className="footer mt-auto py-3 bg-dark">
+        <footer className="footer mt-auto py-3 bg-dark navbar-fixed-bottom">
             <div className="container">
                 <p className="text-light">App desenvolvido por <a href="https://www.linkedin.com/in/wcandil/" target="_blank" rel="noreferrer">Wellington Candil</a></p>
                 <p className="text-light"><small><strong>Sales Dashboard</strong><br />
