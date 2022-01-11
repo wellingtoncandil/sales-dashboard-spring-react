682f162aace8607c939e16f645c38bb1f446761a
Wellington Candil <wcandil34@gmail.com>
2022-01-10 21:11:27 -0300

    Group by search

--- backend/src/main/java/com/wellington/wcvendas/resources/SaleResource.java
+++ backend/src/main/java/com/wellington/wcvendas/resources/SaleResource.java
@@ -1,15 +1,18 @@
 package com.wellington.wcvendas.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.wellington.wcvendas.dto.SaleDTO;
+import com.wellington.wcvendas.dto.SaleSuccessDTO;
+import com.wellington.wcvendas.dto.SaleSumDTO;
 import com.wellington.wcvendas.services.SaleService;
 import com.wellington.wcvendas.services.SellerService;
 
@@ -19,16 +22,22 @@ public class SaleResource {
 
 	@Autowired
 	SaleService service;
-	
-	@Autowired
-	SellerService sellerService;
 
 	@GetMapping
-	@Transactional(readOnly = true)
 	public ResponseEntity<Page<SaleDTO>> findAll(Pageable pageable) {
-		sellerService.findAll();
 		Page<SaleDTO> list = service.findAll(pageable);
 		return ResponseEntity.ok(list);
 	}
 
+	@GetMapping("/sum-by-seller")
+	public ResponseEntity<List<SaleSumDTO>> amountGrupedBySeller() {
+		List<SaleSumDTO> list = service.amountGroupedBySeller();
+		return ResponseEntity.ok(list);
+	}
+	
+	@GetMapping("/success-by-seller")
+	public ResponseEntity<List<SaleSuccessDTO>> successGrupedBySeller() {
+		List<SaleSuccessDTO> list = service.successGroupedBySeller();
+		return ResponseEntity.ok(list);
+	}
 }
--- backend/src/main/java/com/wellington/wcvendas/services/SaleService.java
+++ backend/src/main/java/com/wellington/wcvendas/services/SaleService.java
@@ -1,13 +1,19 @@
 package com.wellington.wcvendas.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.wellington.wcvendas.dto.SaleDTO;
+import com.wellington.wcvendas.dto.SaleSuccessDTO;
+import com.wellington.wcvendas.dto.SaleSumDTO;
 import com.wellington.wcvendas.entities.Sale;
 import com.wellington.wcvendas.repositories.SaleRepository;
+import com.wellington.wcvendas.repositories.SellerRepository;
 
 @Service
 public class SaleService {
@@ -15,9 +21,24 @@ public class SaleService {
 	@Autowired
 	SaleRepository repository;
 	
+	@Autowired
+	SellerRepository sellerRepository;
+	
+	@Transactional(readOnly = true)
 	public Page<SaleDTO> findAll(Pageable pageable){
+		sellerRepository.findAll();
 		Page<Sale> list = repository.findAll(pageable);
 		Page<SaleDTO> listDTO = list.map(x -> new SaleDTO(x));
 		return listDTO;
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSumDTO> amountGroupedBySeller(){
+		return repository.amountGroupedBySeller();
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSuccessDTO> successGroupedBySeller(){
+		return repository.successGroupedBySeller();
 	}
 }
