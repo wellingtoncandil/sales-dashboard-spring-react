ae14f619feb9cd4039bf0c6916b35838bbb89d9c
Wellington Candil <wcandil34@gmail.com>
2022-01-11 14:11:39 -0300

    Datatable integration

--- frontend/src/types/Sale.ts
+++ frontend/src/types/Sale.ts
@@ -1,3 +1,26 @@
+import { Seller } from "./Seller"
+
+export type Sale = {
+    id: number,
+    visited: number,
+    deals: number,
+    amount: number,
+    date: string,
+    seller: Seller;
+}
+
+export type SalePage = {
+    content?: Sale[];
+    last: boolean,
+    totalPages: number,
+    totalElements: number,
+    first: boolean,
+    size?: number,
+    number: number,
+    numberOfElements?: number,
+    empty?: boolean
+}
+
 export type SaleSum = {
     sellerName: string,
     sum: number
